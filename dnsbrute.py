2a8020e932352bd9cc7c9ad16917b1652b3dd268
GIOVANI MARQUES <102267202+DevGiovaniMarques@users.noreply.github.com>
2025-03-29 13:24:03 -0300

    Create dnsbrute.py

---
+++ dnsbrute.py
@@ -0,0 +1,16 @@
+import dns.resolver
+
+resolver = dns.resolver.Resolver()
+wordlist = ['www', 'mail', 'ftp', 'webmail', 'admin', 'test', 'dev', 'blog', 'api', 'secure','shop','advanced','dvwa']
+alvo = 'bancocn.com'
+
+for subdominio in wordlist:
+    try:
+        sub_alvo = "{}.{}".format(subdominio, alvo)
+        resultados = resolver.resolve(sub_alvo, 'A')
+        for resultado in resultados:
+            print("{} -> {}".format(sub_alvo, resultado))
+    except:
+        pass
+
+
